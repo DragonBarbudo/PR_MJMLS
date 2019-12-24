e88654f5ce30f8f2bbca31baa3dd6ad3e1c69053
Albin Rodriguez <ealbinu@gmail.com>
2019-12-24 13:35:19 -0600

    Bootstrap & PuntosPPG

--- BootstrapBlue/app/app.js
+++ BootstrapBlue/app/app.js
@@ -1,9 +1,11 @@
 $doc = $(document)
 /* MENU - NAVBAR */
 $doc.scroll(function () {
-  if($doc.scrollTop() >= 200) {
+  if($doc.scrollTop() >= 400) {
     $('nav.navbar').addClass('sticky-top animated slideInDown')
+    $('body').addClass('with-stick')
   } else {
     $('nav.navbar').removeClass('sticky-top animated slideInDown')
+    $('body').removeClass('with-stick')
   }
 })
